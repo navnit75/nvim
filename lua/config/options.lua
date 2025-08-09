43377f476902276759fdfc10277386291c63ed4a
Navnit Kumar <60180825+navnit75@users.noreply.github.com>
2025-08-09 00:08:31 -0400

    Add files via upload

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.tabstop = 4        -- Width of tab character
+vim.opt.softtabstop = 4    -- Fine tunes the amount of whitespace to be added
+vim.opt.shiftwidth = 4     -- Number of spaces to use for autoindenting
+vim.opt.expandtab = true   -- Expand tabs to spaces
+vim.opt.smarttab = true    -- Insert tabs according to shiftwidth
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        config = function()
+            require("mason")
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            automatic_enable = {
+                exclude = {
+                    "jdtls",
+                },
+            },
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "jdtls",
+                    "eslint",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+    { "mfussenegger/nvim-jdtls" },
+}
