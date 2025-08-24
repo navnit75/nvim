f981dfdb323042431e819c31473eca3cb2bea3b0
Navnit Kumar <knavnav@amazon.com>
2025-08-24 00:54:03 -0400

    Added few plugins and extra configuration

--- init.lua
+++ init.lua
@@ -2,12 +2,33 @@ vim.g.mapleader = " "
 require("config.lazy")
 require("mason").setup()
 -- require("rose-pine").setup({
---   styles = {
---     italic = false,
---   },
+--     styles = {
+--         italic = true,
+--     },
 -- })
-vim.cmd.colorscheme("rose-pine")
+-- vim.cmd.colorscheme('OceanicNext')
+-- vim.cmd([[
+--                 hi Normal guibg=NONE ctermbg=NONE
+--                 hi LineNr guibg=NONE ctermbg=NONE
+--                 hi SignColumn guibg=NONE ctermbg=NONE
+--                 hi EndOfBuffer guibg=NONE ctermbg=NONE
+--         ]])
+-- vim.cmd.colorscheme('rose-pine')
+
+-- AYU SETUP
+-- require('ayu').setup({
+--     mirage = false,
+--     terminal = true,
+--     overrides = {}
+-- })
+-- vim.cmd.colorscheme("ayu")
+vim.cmd.colorscheme("moonfly")
 vim.opt.termguicolors = true
+
+vim.opt.formatoptions:remove("c")
+vim.opt.formatoptions:remove("r")
+vim.opt.formatoptions:remove("o")
+
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
@@ -27,7 +48,12 @@ config.setup({
 require("toggleterm").setup({
     size = 100,
     open_mapping = [[<C-t>]],
-    direction = "vertical",
+    direction = "float",
+    float_opts = {
+        border = "curved",
+        width = 100,
+        height = 20,
+    },
     shade_terminals = true,
     terminal_mappings = true,
     insert_mappings = true,
@@ -44,3 +70,31 @@ vim.api.nvim_create_autocmd("FileType", {
         require("jdtls.jdtls_setup").setup()
     end,
 })
+
+-- CONFORM
+local conform = require("conform")
+conform.setup({
+    formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        liquid = { "prettier" },
+        python = { "isort", "black" },
+    },
+})
+
+vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+    conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+    })
+end, { desc = "Format file or range (in visual mode)" })
