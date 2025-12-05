4cc97dd609d3e7fef2b834b1d995fad94ebca864
navnit75 <kumar.navnit4175@gmail.com>
2025-12-04 22:10:29 -0500

    Added something else

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -30,9 +30,10 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
-            lspconfig.ts_ls.setup({})  -- Setup TypeScript LSP
+            lspconfig.ts_ls.setup({}) -- Setup TypeScript LSP
             lspconfig.jdtls.setup({})
             lspconfig.smithy_ls.setup({})
+            lspconfig.pylsp.setup({})
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
