6842e27c9038971e57ad711130fcdc3cbedb34d6
navnit75 <kumar.navnit4175@gmail.com>
2025-12-07 13:53:04 -0500

    Added theme

--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -7,7 +7,7 @@ return {
         -- require("github-theme").setup({
         -- ...
         -- })
-
-        vim.cmd("colorscheme github_dark_default")
+        vim.cmd("colorscheme github_dark_high_contrast")
+        -- vim.cmd("colorscheme github_dark_default")
     end,
 }
